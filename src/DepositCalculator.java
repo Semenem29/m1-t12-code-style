e44e5e00b62f9242dea17130f91fd94609165069
Мария <mashunia0306@yandex.ru>
2023-04-09 07:36:37 +0300

    Мария Николаевна проверила Ваше домашнее задание))) Оценка 5-))))

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -6,7 +6,10 @@ public class DepositCalculator {
         new DepositCalculator().manageCalculation();
     }
 
-    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) { /*TODO
+    Я бы переменную depositPeriod свядала бы со словом год, т.к. в нее не получится положить
+    другой период (например месяцы или кварталы).
+    Переменной amount тоже можно найти более конкретное название))))*/
         double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
 
         return roundValue(pay, 2);
@@ -20,21 +23,21 @@ public class DepositCalculator {
     double roundValue(double value, int places) {
         double ScaLe = Math.pow(10, places);
 
-        return Math.round(value * ScaLe) / ScaLe;
+        return Math.round(value * ScaLe) / ScaLe; //TODO в переменной scaLe должны быть все буквы строчными:D
     }
 
     void manageCalculation() {
 
-        int period;
+        int period; //TODO Также, считаю, что можно связать имя переменной со словом год)
         int action;
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Введите сумму вклада в рублях:");
         int amount = scanner.nextInt();
-        
+
         System.out.println("Введите срок вклада в годах:");
         period = scanner.nextInt();
-        
+        //TODO Нужно ли пропускать тут строки?? Ну в любом случае, так красивее))))
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
         
